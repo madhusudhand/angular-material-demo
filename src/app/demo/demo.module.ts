93506642f9c2d11490ef352326340dbe0230801a
madhusudhand <madhusudhan.dollu@gmail.com>
2017-04-26 22:01:29 +0530

    adding basic header and sidenav

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoComponent } from './demo.component';
+import { HeaderComponent } from './header/header.component';
+import { MdToolbarModule, MdButtonModule, MdIconModule, MaterialModule } from '@angular/material';
+import { TabsComponent } from './tabs/tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    // MdToolbarModule,
+    // MdButtonModule,
+    // MdIconModule,
+    MaterialModule
   ],
-  declarations: [DemoComponent]
+  declarations: [DemoComponent, HeaderComponent, TabsComponent, SidenavComponent]
 })
 export class DemoModule { }
